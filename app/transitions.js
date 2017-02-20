5797f59899daba75bba2fea1e5f25ba104ea8e59
Tobi Adeyinka <tobex9434@yahoo.com>
2017-02-20 07:28:08 +0100

    index view sub-name links impl

--- app/transitions.js
+++ app/transitions.js
@@ -1,2 +1,22 @@
 export default function(){  
+
+    /*index page transitions*/
+    this.transition(
+        this.fromRoute('index'),
+        this.toRoute('about'),
+        this.use('fade')
+    );
+
+    this.transition(
+        this.fromRoute('index'),
+        this.toRoute('projects'),
+        this.use('fade')
+    );
+
+    this.transition(
+        this.fromRoute('index'),
+        this.toRoute('contact'),
+        this.use('fade')
+    );
+
 }
