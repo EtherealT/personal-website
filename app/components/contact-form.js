32cf67b592a438ea6b1fc21b969184d23067624a
Tobi Adeyinka <tobex9434@yahoo.com>
2017-02-22 12:00:41 +0100

    mod to message posting

--- app/components/contact-form.js
+++ app/components/contact-form.js
@@ -32,15 +32,13 @@ export default Ember.Component.extend({
 
                 // send message
                 Ember.$.post('/message.php', {'name':senderName, 'email':senderEmail, 'message':message}, function(data){
-                    if(data == "mail sent"){
-                        Ember.$('#result-div').append('<div style="font-family:hp simplified regular;background-color:#43d58c;color:#ffffff;text-align:center;width:250px;height:10px;border-radius:5px;padding:10px;padding-top:5px;">Thanks for your message!</div>');
-                        Ember.$('#result-div').fadeIn(500, 'swing');
+                    Ember.$('#result-div').append('<div style="font-family:hp simplified regular;background-color:#43d58c;color:#ffffff;text-align:center;width:250px;height:10px;border-radius:5px;padding:10px;padding-top:5px;">Thanks for your message!</div>');
+                    Ember.$('#result-div').fadeIn(500, 'swing');
 
-                        setTimeout(
-                            function() {
-                                Ember.$('#result-div').fadeOut(500, 'swing');
-                        }, 3000);
-                    }
+                    setTimeout(
+                        function() {
+                            Ember.$('#result-div').fadeOut(500, 'swing');
+                    }, 3000);
                 });
             }
         }
