c0caae18f80400ab6de29b48b4bb7a87989a83e3
Tobi Adeyinka <tobex9434@yahoo.com>
2017-03-26 16:59:41 +0200

    add tags attribute to project objects

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -7,28 +7,32 @@ let projects = [
         title: 'Veedigo',
         description: 'Veedigo is a platform engineered to help college students in Budapest Hungary, by providing some services that aid student life. This is a collaborative project with Harry Otemelate.',
         url: 'http://veedigo.com',
-        imgUrl: 'assets/img/veedigo.png'
+        imgUrl: 'assets/img/veedigo.png',
+        tags: ['java', 'php', 'javascript', 'ember']
     },
 
     {
         title: '2 Lies 1 Fact',
         description: 'A general knowledge quiz game on android. The source code is available on github.',
         url: 'https://play.google.com/store/apps/details?id=com.nectarmicrosystems.libgdx.twoliesonefact.android',
-        imgUrl: 'assets/img/2l1f.jpeg'
+        imgUrl: 'assets/img/2l1f.jpeg',
+        tags: ['java', 'android', 'libgdx']
     },
 
     {
         title: 'Beam',
         description: 'Beam is a small text editor I\'m developing as a side project in my free time. I intend to implement features such as text highlighting, FTP connections and a few others, the source is hosted on Github.',
         url: 'https://github.com/tobex9434/Beam',
-        imgUrl: 'assets/img/beam.jpg'
+        imgUrl: 'assets/img/beam.jpg',
+        tags: ['java']
     },
 
     {
         title: 'Expense Journal',
         description: 'Expense journal is a minimal android application for tracking personal finance. It is open sourced on Github.',
         url: 'https://play.google.com/store/apps/details?id=com.nectarmicrosystems.fundsmanager',
-        imgUrl: 'assets/img/ej.jpg'
+        imgUrl: 'assets/img/ej.jpg',
+        tags: ['java', 'android', 'libgdx']
     }
 ];
 
