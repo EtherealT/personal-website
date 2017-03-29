89bfda7acda6d35d0beced31d971f67c5ec6a4d0
Tobi Adeyinka <tobex9434@yahoo.com>
2017-03-29 22:31:49 +0200

    disable page fadein

--- app/mixins/page-fadein.js
+++ app/mixins/page-fadein.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
     activate () {
         Ember.run.scheduleOnce('afterRender', this, function() {
-            Ember.$('#page-wrapper').fadeIn(400);
+            //Ember.$('#page-wrapper').fadeIn(400);
         });
     }
 });
