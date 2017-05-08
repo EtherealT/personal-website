46edf7996ff4b8e861cfb998bcabddd429b89b26
Tobi Adeyinka <tobex9434@yahoo.com>
2017-05-08 16:50:22 +0200

    add page adder to books page

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
+let pageName = 'Favorite Books';
+let books = [
+];
+
 export default Ember.Route.extend({
+    model(){
+        return{
+            pageName: pageName,
+            books: books
+        };
+    }
 });
