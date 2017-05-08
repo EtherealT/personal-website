c0e6b1fb97037de0016bc87192883c125cdef999
Tobi Adeyinka <tobex9434@yahoo.com>
2017-05-08 17:35:37 +0200

    complete books page

--- app/routes/books.js
+++ app/routes/books.js
@@ -2,6 +2,30 @@ import Ember from 'ember';
 
 let pageName = 'Favorite Books';
 let books = [
+    {
+        title: 'Blink',
+        url: '<a target="_blank"  href="https://www.amazon.com/gp/product/0316010669/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0316010669&linkCode=as2&tag=tobex9434-20&linkId=8681a375e00f316847990f72129a70d9"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=0316010669&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=tobex9434-20" ></a><img src="//ir-na.amazon-adsystem.com/e/ir?t=tobex9434-20&l=am2&o=1&a=0316010669" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />'
+    },
+
+    {
+        title: 'How to win friends and influence people',
+        url: '<a target="_blank"  href="https://www.amazon.com/gp/product/0671027034/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0671027034&linkCode=as2&tag=tobex9434-20&linkId=892bcab77a7a819ec3320fb3c7f571d9"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=0671027034&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=tobex9434-20" ></a><img src="//ir-na.amazon-adsystem.com/e/ir?t=tobex9434-20&l=am2&o=1&a=0671027034" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />'
+    },
+
+    {
+        title: '7 habits of highly effective people',
+        url: '<a target="_blank"  href="https://www.amazon.com/gp/product/1451639619/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1451639619&linkCode=as2&tag=tobex9434-20&linkId=4ddb3e17e19d2fefc8c1bb1d22c173bc"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=1451639619&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=tobex9434-20" ></a><img src="//ir-na.amazon-adsystem.com/e/ir?t=tobex9434-20&l=am2&o=1&a=1451639619" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />'
+    },
+
+    {
+        title: 'Sapiens: a brief history of humans',
+        url: '<a target="_blank"  href="https://www.amazon.com/gp/product/0062316095/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0062316095&linkCode=as2&tag=tobex9434-20&linkId=c803069c11a0428dafc91aace8f1de3c"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=0062316095&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=tobex9434-20" ></a><img src="//ir-na.amazon-adsystem.com/e/ir?t=tobex9434-20&l=am2&o=1&a=0062316095" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />'
+    },
+
+    {
+        title: 'David & Goliath',
+        url: '<a target="_blank"  href="https://www.amazon.com/gp/product/0316204374/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0316204374&linkCode=as2&tag=tobex9434-20&linkId=2105e61aa831e10dccac3f0f45be69e6"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=0316204374&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=tobex9434-20" ></a><img src="//ir-na.amazon-adsystem.com/e/ir?t=tobex9434-20&l=am2&o=1&a=0316204374" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />'
+    }
 ];
 
 export default Ember.Route.extend({
