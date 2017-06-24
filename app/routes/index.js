2b983d02e98c6b429032c60faf31e1a73c8ce4a3
Tobi Adeyinka <tobex9434@yahoo.com>
2017-06-24 21:22:29 +0200

    working on preloading images

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,39 @@
 import Ember from 'ember';
 import PageFadein  from '../mixins/page-fadein';
 
+let imgDir = '/assets/img/';
+let preloadedImages = [];
+let imgFiles = [
+    '2l1f.jpeg',
+    'beam.jpg',
+    'codepen.png',
+    'ej.jpg',
+    'favicon-32x32.png',
+    'github.png',
+    'li.png',
+    'me.jpeg',
+    'movies.jpg',
+    'stack-o.png',
+    'stock.jpg',
+    'twitter.png',
+    'veedigo.png',
+    'vizerr.png'
+];
+
 export default Ember.Route.extend(PageFadein, {
+
+    activate: function(){
+        this.preloadAllImages();
+    },
+
+    preloadAllImages: function(){
+        var iter = 0;
+        var imageCount = imgFiles.length;
+
+        for(iter; iter < imageCount; iter++){
+            var imageSource = imgDir + imgFiles[iter];
+            preloadedImages.push((new Image()).src = imageSource);
+        }
+    }
+
 });
